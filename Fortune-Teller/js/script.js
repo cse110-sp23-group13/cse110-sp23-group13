35f37d81c7c13c8d9f6e0faa94da792957d8ee0b
Cat1029 <44483272+Cat1029@users.noreply.github.com>
2023-05-16 14:27:38 -0700

    add textbox

---
+++ Fortune-Teller/js/script.js
@@ -0,0 +1,8 @@
+/*document.getElementById('c0').addEventListener("click", chooseCard);
+function chooseCard(){
+    document.querySelector('#c0').style.display='none';
+    document.getElementById('c0').style.visibility = 'hidden';
+
+}
+chooseCard();
+confusion....*/
