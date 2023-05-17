0c5c140834c3df765ac21242b348850a4b43674a
Cat1029 <44483272+Cat1029@users.noreply.github.com>
2023-05-16 17:33:23 -0700

    select cards

--- Fortune-Teller/js/script.js
+++ Fortune-Teller/js/script.js
@@ -1,8 +1,23 @@
-/*document.getElementById('c0').addEventListener("click", chooseCard);
-function chooseCard(){
-    document.querySelector('#c0').style.display='none';
-    document.getElementById('c0').style.visibility = 'hidden';
-
-}
-chooseCard();
-confusion....*/
+document.addEventListener('DOMContentLoaded', function() {
+    /* Check when card is clicked*/
+    document.getElementById('deck').addEventListener('click', function(event) {
+        if (event.target.classList.contains('card')) {
+            /* hide the card */
+            event.target.style.visibility = 'hidden';
+            console.log('clicked');
+            var num = Math.floor(Math.random() * 2);
+            var image = new Image(100,200);
+            if (num == 0){
+                image.src = "./images/thefool.svg";
+            }else{
+                image.src = "./images/themagician.svg";
+            }
+            /* at the bottom */
+            document.body.appendChild(image);
+            /* at the top
+            let parent = document.body;
+            let child = parent.firstChild;
+            parent.insertBefore(image, child); */
+        }
+      });
+});
