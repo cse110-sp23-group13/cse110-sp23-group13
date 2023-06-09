0017fc61c0b994eee600822048935568789bede1
Molly <mojeanmac@gmail.com>
2023-06-08 22:58:18 -0700

    will github accept localhost?

--- Fortune-Teller/js/__tests__/EndToEnd.test.js
+++ Fortune-Teller/js/__tests__/EndToEnd.test.js
@@ -1,23 +1,25 @@
 describe('Basic user flow for Website', () => {
     // First, visit the website
     beforeAll(async () => {
-      await page.goto('https://cse110-sp23-group13.github.io/cse110-sp23-group13/Fortune-Teller/');
+      await page.goto('http://127.0.0.1:5500/Fortune-Teller/index.html');
     });
   
     // Next, Click on the black cat logo to check if it switches to the option page
     it('Clicking the black cat logo', async () => {
       console.log('Waiting to click the black logo');
       // Query select the black cat logo
-      const blackCatLogo = await page.$('.clicker');
+      const blackCatLogo = await page.$('#landing-logo');
       
     
-      // Once you have the button, you can click it and check the innerText property of the button.
+      // Once you have the button, you can click it
       await blackCatLogo.click();
 
-      let currentDomain = window.location.href;
+      await page.waitForNavigation();
+
+      let currentDomain = page.url();
 
       // Expect page to be the option.html page
-      expect(currentDomain).toBe('https://cse110-sp23-group13.github.io/cse110-sp23-group13/Fortune-Teller/option.html');
+      expect(currentDomain).toBe('http://127.0.0.1:5500/Fortune-Teller/option.html');
     });
 
   });
